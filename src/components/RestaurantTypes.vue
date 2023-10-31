3e632fe7269e5d69089c3d027106535b64bd4327
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-10-31 11:56:45 +0100

    add home and carousel

---
+++ src/components/RestaurantTypes.vue
@@ -0,0 +1,271 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <!-- slider sm -->
+    <div class="container-fluid container-p-s py-5 d-md-none d-lg-none">
+        <div id="carouselExampleAutoplaying" class="carousel slide d-block d-md-none d-lg-none" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <a href="#0" class="card">
+                        <img src="../assets/images/Type/icon/hamburger.png" class="card-img-top" alt="">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="card-text text-center">Hamburger</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#0" class="card">
+                        <img src="../assets/images/Type/icon/hamburger.png" class="card-img-top" alt="">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="card-text text-center">Hamburger</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#0" class="card">
+                        <img src="../assets/images/Type/icon/hamburger.png" class="card-img-top" alt="">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="card-text text-center">Hamburger</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="prev">
+                <span><i class="fa-solid fa-chevron-left"></i></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="next">
+                <span><i class="fa-solid fa-chevron-right"></i></span>
+            </button>
+        </div>
+    </div>
+
+    <!-- slider md -->
+    <div class="container-fluid p-5 d-none d-md-block d-lg-none">
+        <div id="carouselExampleInterval" class="carousel slide d-none d-sm-none d-md-block d-lg-block"
+            data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row g-3 justify-content-center">
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/hamburger.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">Hamburger</h3>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/fried-potatoes.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">Fritture</h3>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/carne.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">BBQ</h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item active">
+                    <div class="row g-3 justify-content-center">
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/hamburger.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">Hamburger</h3>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/fried-potatoes.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">Fritture</h3>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/carne.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">BBQ</h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item active">
+                    <div class="row g-3 justify-content-center">
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/hamburger.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">Hamburger</h3>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/fried-potatoes.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">Fritture</h3>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm col-md-3 col-lg-3">
+                            <a href="#0" class="card">
+                                <img src="../assets/images/Type/icon/carne.png" class="card-img-top" alt="">
+                                <div class="card-body d-flex align-items-center justify-content-center">
+                                    <h3 class="card-text text-center">BBQ</h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                data-bs-slide="prev">
+                <span><i class="fa-solid fa-chevron-left"></i></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                data-bs-slide="next">
+                <span><i class="fa-solid fa-chevron-right"></i></span>
+            </button>
+        </div>
+    </div>
+
+    <!-- card in lg -->
+    <div class="container-fluid">
+        <div class="container d-none d-md-none d-lg-flex py-5 gap-5 justify-content-center">
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/carne.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">BBQ</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/dolci.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">dolci</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/fish.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">pesce</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/fried-potatoes.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">fritti</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/hamburger.png" class="card-img-top my-width" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">hamburger</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/kebab.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">kebab</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/pizza.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">pizza</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/salad.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">salad</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/sandwich.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">sandwich</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/spaghetti.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">spaghetti</h3>
+                </div>
+            </a>
+            <a href="#0" class="card">
+                <img src="../assets/images/Type/icon/sushi.png" class="card-img-top" alt="">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h3 class="card-text text-center">sushi</h3>
+                </div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container-fluid {
+    padding: 0 100px;
+
+    .card {
+        border: 0;
+        text-decoration: none;
+
+        img {
+            height: 100px;
+            object-fit: contain;
+            object-position: center;
+        }
+
+        .card-img-overlay {
+            color: white;
+        }
+
+        h3 {
+            font-size: 1.2rem;
+        }
+
+        .my-width {
+            max-width: 71px;
+        }
+    }
+
+    .carousel-control-next {
+        transform: translate(50%, -50%);
+    }
+
+    .carousel-control-prev {
+        transform: translate(-50%, -50%);
+    }
+
+    .carousel-control-next,
+    .carousel-control-prev {
+        width: 50px;
+        border-radius: 50%;
+        height: 50px;
+        top: 50%;
+        background-color: black;
+    }
+}
+
+.container-p-s {
+    padding: 0 50px;
+}
+</style>
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,23 @@
+<script>
+import TheFooter from "./components/TheFooter.vue";
+import TheHeader from "./components/TheHeader.vue";
+import RestaurantTypes from './components/RestaurantTypes.vue';
+
+
+export default {
+    components: {
+        TheHeader,
+        TheMain,
+        TheFooter,
+        RestaurantTypes,
+    },
+};
+</script>
+
+<template>
+    <TheHeader></TheHeader>
+    <RestaurantTypes />
+    <TheFooter></TheFooter>
+</template>
+
+<style lang="scss" scoped></style>
