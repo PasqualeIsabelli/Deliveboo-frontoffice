f26c94df5ba928eaea69933816a4c7a92146d27b
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-11-05 16:44:52 +0100

    Done for today

--- src/pages/Restaurant.vue
+++ src/pages/Restaurant.vue
@@ -15,10 +15,15 @@ export default {
         fetchData() {
             axios.get('http://127.0.0.1:8000/api/restaurants/' + this.$route.params.id)
                 .then((response) => {
-                    this.restaurant = response.data;
+                    this.restaurant = response.data.restaurant;
                     console.log(this.restaurant);
                 })
+        },
+
+        getImg(type) {
+            return `http://127.0.0.1:8000/storage/${type.img}`;
         }
+
     },
     mounted() { 
         this.fetchData();
@@ -27,6 +32,26 @@ export default {
 </script>
 
 <template>
+
+    <div class="container">
+
+        <h3>{{ restaurant.activity_name }}</h3>
+
+        <img :src="getImg(restaurant)" alt="">
+
+        <ul v-for="product in restaurant.products">
+        
+            <li class="card" style="width: 18rem;">
+                <img :src="getImg(product)" class="card-img-top" alt="Img prodotto">
+                <div class="card-body">
+                    <h5 class="card-title">{{ product.name }}</h5>
+                    <p class="card-text">{{ product.description }}</p>
+                </div>
+            </li>
+        
+        </ul>
+
+    </div>
 
 </template>
 
