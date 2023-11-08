e3ae44cde37d44fc4b181b266446f6284aacfe7f
Salvatore Bono <salvatorebono2001@gmail.com>
2023-11-08 14:59:59 +0100

    addItem counter

--- src/pages/Restaurant.vue
+++ src/pages/Restaurant.vue
@@ -1,64 +1,63 @@
 <script>
-
 import axios from "axios";
 
 export default {
-
   components: {},
 
   data() {
-
     return {
       items: [],
       restaurant: [],
-      counters: {},
-      counter: 0
+      cart: {},
     };
-
   },
 
   computed: {
-
     itemsFromLocalStorage() {
       // Recupera gli elementi dal localStorage e analizza il JSON, se presente
       return JSON.parse(localStorage.getItem("items") || "[]");
     },
-
   },
 
   methods: {
     localStorage() {
       // Salva gli elementi nel localStorage
       return localStorage.setItem("items", JSON.stringify(this.items));
-
     },
     fetchData() {
       // Effettua la chiamata API per ottenere i dati del ristorante
-      axios.get("http://127.0.0.1:8000/api/restaurants/" + this.$route.params.id)
+      axios
+        .get("http://127.0.0.1:8000/api/restaurants/" + this.$route.params.id)
         .then((response) => {
           this.restaurant = response.data.restaurant;
         });
-
     },
-
+    // Questo è il metodo che gestisce l'aggiunta di un prodotto al carrello
     addItem(product) {
-      // Verifica se il prodotto con lo stesso ID esiste già nell'array items
-      const existingProductIndex = this.items.findIndex(item => item.id === product.id);
+      // Trova l'indice del prodotto nel carrello se esiste
+      const existingProductIndex = this.items.findIndex(
+        (item) => item.id === product.id
+      );
 
+      // Verifica se il prodotto esiste già nel carrello
       if (existingProductIndex !== -1) {
-        // Se il prodotto esiste già, incrementa il contatore corrispondente nell'oggetto counters
-        const productId = product.id;
-        this.counters[productId]++;
+        // Se il prodotto esiste già, ottieni il prodotto esistente
+        const existingProduct = this.items[existingProductIndex];
+
+        // Verifica se c'è già un contatore associato a questo prodotto nel carrello
+        if (this.cart[existingProduct.id]) {
+          // Se sì, incrementa il contatore per questo prodotto
+          this.cart[existingProduct.id]++;
+        } else {
+          // Se non esiste un contatore per questo prodotto, inizializza il contatore a 1
+          this.cart[existingProduct.id] = 1;
+        }
       } else {
-        // Altrimenti, aggiungi il prodotto all'array items e inizializza il contatore a 1
+        // Se il prodotto non esiste ancora nel carrello, aggiungi il prodotto all'array items
         this.items.push(product);
+        // Inizializza un contatore a 1 nell'oggetto cart per questo nuovo prodotto nel carrello
+        this.cart[product.id] = 1;
       }
-
-      // Aggiorna il valore totale del counter
-      this.counter = Object.values(this.counters).reduce((total, count) => total + count, 0);
-
-      console.log(this.counter);
-      console.log(this.items);
 
       // Salva nel localStorage
       this.localStorage();
@@ -69,15 +68,11 @@ export default {
       this.items.splice(index, 1);
 
       this.localStorage();
-
     },
 
     getImg(type) {
-
       return `http://127.0.0.1:8000/storage/${type.img}`;
-
     },
-
   },
 
   mounted() {
@@ -85,23 +80,17 @@ export default {
     if (this.items.length > 0) {
       // Accedi al primo elemento e confronta il restaurant_id
       if (this.items[0].restaurant_id != this.$route.params.id) {
-
         this.items = [];
 
         this.localStorage();
-
       }
-
     }
 
     this.fetchData();
-
   },
 
   created() {
-
     this.items = this.itemsFromLocalStorage;
-
   },
 };
 </script>
@@ -113,9 +102,17 @@ export default {
       <div class="col-8">
         <h2 class="py-3">{{ restaurant.activity_name }}</h2>
         <div class="row row-cols-md-2 g-3 pb-5">
-          <div class="col" v-for="product in restaurant.products" v-show="product.visible == 1">
+          <div
+            class="col"
+            v-for="product in restaurant.products"
+            v-show="product.visible == 1"
+          >
             <div class="my-card">
-              <img :src="getImg(product)" class="my-card-img" alt="Img prodotto" />
+              <img
+                :src="getImg(product)"
+                class="my-card-img"
+                alt="Img prodotto"
+              />
               <div class="my-text d-flex flex-column justify-content-between">
                 <h5>{{ product.name }}</h5>
                 <p>{{ product.description }}</p>
@@ -131,19 +128,24 @@ export default {
       </div>
 
       <div class="col-4">
-        <div v-if="(items.length > 0)">
+        <div v-if="items.length > 0">
           <h2 class="py-3">Il tuo carrello</h2>
           <div class="my-table-container p-3">
             <table class="table table-borderless m-0">
               <tbody v-for="(item, index) in items" :key="index">
                 <tr>
+                  <td class="text-center">
+                    {{ cart[item.id] }}
+                  </td>
                   <td>
                     <h5 class="card-title">{{ item.name }}</h5>
                   </td>
                   <td class="text-center">{{ item.price }}€</td>
-                  <td class="text-center">{{ counters[item.id] }}</td>
                   <td>
-                    <button class="btn text-danger p-0" @click="removeItem(index)">
+                    <button
+                      class="btn text-danger p-0"
+                      @click="removeItem(index)"
+                    >
                       <i class="fa-solid fa-trash"></i>
                     </button>
                   </td>
@@ -170,7 +172,7 @@ export default {
 .my-card {
   min-width: 200px;
   max-height: 100%;
-  background-color: #02CCBC;
+  background-color: #02ccbc;
   border-radius: 10px;
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.24);
   font-size: 16px;
@@ -200,7 +202,6 @@ export default {
   letter-spacing: 2px;
   height: 180px;
 }
-
 
 .my-table-container {
   border: 1px solid lightgrey;
