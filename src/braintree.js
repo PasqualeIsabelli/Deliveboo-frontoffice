13a436760c3bd9c7f60c1c4eca5d26524b1b0561
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-11-08 16:57:34 +0100

    Indentation done

--- src/braintree.js
+++ src/braintree.js
@@ -3,44 +3,48 @@ var $errBox = $('#error-box');
 var $successBox = $('#success-box');
 
 function displayMessage(box, message) {
-  box.html(message);
-  box.removeClass('hidden');
+
+    box.html(message);
+    box.removeClass('hidden');
+
 }
 
 function clearMessage(box) {
-  box.html('');
-  box.addClass('hidden');
+
+    box.html('');
+    box.addClass('hidden');
+
 }
 
 braintree.dropin.create({
-  authorization: 'sandbox_fw2yzmcx_8g9hfnsjn3sxvt5r',
-  selector: '#dropin-container',
-  card: {
-          overrides: {
+    authorization: 'sandbox_fw2yzmcx_8g9hfnsjn3sxvt5r',
+    selector: '#dropin-container',
+    card: {
+        overrides: {
             fields: {
-              number: {
-                supportedCardBrands: {
-                },
-              }
+                number: {
+                    supportedCardBrands: {
+                    },
+                }
             }
-          }
-        },
+        }
+    },
 }, function (createErr, instance) {
-  if (createErr) {
-    displayMessage($errBox, createErr.message);
-    return;
-  }
-  button.addEventListener('click', function () {
+        if (createErr) {
+            displayMessage($errBox, createErr.message);
+            return;
+        }
+    button.addEventListener('click', function () {
     clearMessage($errBox);
     clearMessage($successBox);
     
-    instance.requestPaymentMethod(function (requestPaymentMethodErr, payload) {
-      if (requestPaymentMethodErr) {
-        displayMessage($errBox, requestPaymentMethodErr.message);
-        return;
-      }
-      
-      displayMessage($successBox, 'Send Payment Method Nonce (' + payload.nonce + ') to your server.');
+        instance.requestPaymentMethod(function (requestPaymentMethodErr, payload) {
+            if (requestPaymentMethodErr) {
+                displayMessage($errBox, requestPaymentMethodErr.message);
+                return;
+            }
+
+            displayMessage($successBox, 'Send Payment Method Nonce (' + payload.nonce + ') to your server.');
+        });
     });
-  });
 });
